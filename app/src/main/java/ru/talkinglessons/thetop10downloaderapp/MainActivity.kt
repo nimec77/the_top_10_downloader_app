298e4442c1ff800401f6ecb1b844200edcfa2cb8
comrade77 <nimec77@gmail.com>
2021-02-07 15:42:37 +0700

    Initial Commit

---
+++ app/src/main/java/ru/talkinglessons/thetop10downloaderapp/MainActivity.kt
@@ -0,0 +1,15 @@
+package ru.talkinglessons.thetop10downloaderapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    private suspend fun downloadData() {
+
+    }
+}
