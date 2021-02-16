9790bf7413497b668cc602d73ba15260b806d03f
comrade77 <nimec77@gmail.com>
2021-02-16 20:33:44 +0700

    Закончил №19

--- app/src/main/java/ru/talkinglessons/thetop10downloaderapp/adapter/FeedAdapter.kt
+++ app/src/main/java/ru/talkinglessons/thetop10downloaderapp/adapter/FeedAdapter.kt
@@ -10,6 +10,12 @@ import android.widget.TextView
 import ru.talkinglessons.thetop10downloaderapp.R
 import ru.talkinglessons.thetop10downloaderapp.entity.FeedEntry
 
+class ViewHolder(v: View) {
+    val tvName: TextView = v.findViewById(R.id.tvName)
+    val tvArtist: TextView = v.findViewById(R.id.tvArtist)
+    val tvSummary: TextView = v.findViewById(R.id.tvSummary)
+}
+
 class FeedAdapter(
     context: Context,
     private val resource: Int,
@@ -27,17 +33,23 @@ class FeedAdapter(
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         Log.d(tag, "getView() called")
 
-        val view = convertView ?: inflater.inflate(resource, parent, false)
+        val view: View
+        val viewHolder: ViewHolder
 
-        val tvName: TextView = view.findViewById(R.id.tvName)
-        val tvArtists: TextView = view.findViewById(R.id.tvArtist)
-        val tvSummary: TextView = view.findViewById(R.id.tvSummary)
+        if (convertView == null) {
+            view = inflater.inflate(resource, parent, false)
+            viewHolder = ViewHolder(view)
+            view.tag = viewHolder
+        } else {
+            view = convertView
+            viewHolder = view.tag as ViewHolder
+        }
 
         val currentApp = applications[position]
 
-        tvName.text = currentApp.name
-        tvArtists.text = currentApp.artist
-        tvSummary.text = currentApp.summary
+        viewHolder.tvName.text = currentApp.name
+        viewHolder.tvArtist.text = currentApp.artist
+        viewHolder.tvSummary.text = currentApp.summary
 
         return view
     }
